7330aa85f3f86c2ac5e95a19050f6dcef3c192e4
michaeladilulo <michaeladilulo@gmail.com>
2023-07-07 19:50:31 -0600

    Fischer Yates Shuffle Algorithm, shuffle cards

--- app.js
+++ app.js
@@ -53,3 +53,16 @@ const myNewDeck = {
             return cards;
         }
 }
+
+// Fischer Yates Shuffle (used to shuffle array)
+
+function shuffle(arr) {
+    // loop over array backwards
+    for(let i = arr.length - 1; i > 0; i--) {
+        // pick random index before current element
+        let j = Math.floor(Math.random() * (i + 1))
+        // swap
+        [arr[i], arr[j]] = [arr[j], [arr[i]]]
+        console.log(arr);
+    }
+}
