4c69366f9507a7aafa21ac4fc47f3acacf6fb470
michaeladilulo <michaeladilulo@gmail.com>
2023-07-07 19:24:56 -0600

    Return deck and adjusted cards in deck start 2

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 function makeDeck() {
     const deck = [];
     const suits = ['hearts', 'diamonds', 'clubs', 'spades'];
-    const values = '1,2,3,4,5,6,7,8,9,10,J,Q,K,A'
+    const values = '2,3,4,5,6,7,8,9,10,J,Q,K,A'
 
     for(let value of values.split(',')) {
         for(let suit of suits) {
@@ -11,4 +11,5 @@ function makeDeck() {
             })
         }
     }
+    return deck;
 }
