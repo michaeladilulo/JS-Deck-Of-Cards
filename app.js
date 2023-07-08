d43bf4e5cd971b4d2e34b7271697429d2b52c2e6
michaeladilulo <michaeladilulo@gmail.com>
2023-07-07 19:53:48 -0600

    Shuffle added to Object

--- app.js
+++ app.js
@@ -51,18 +51,19 @@ const myNewDeck = {
                 this.drawCard(this.drawSingleCard());
             }
             return cards;
+        },
+
+        // Fischer Yates Shuffle (used to shuffle array)
+
+        shuffle() {
+            const {deck} = this;
+                // loop over array backwards
+            for(let i = arr.length - 1; i > 0; i--) {
+                // pick random index before current element
+            let j = Math.floor(Math.random() * (i + 1))
+                // shuffle
+            [arr[i], arr[j]] = [arr[j], [arr[i]]]
+            console.log(arr);
         }
-}
-
-// Fischer Yates Shuffle (used to shuffle array)
-
-function shuffle(arr) {
-    // loop over array backwards
-    for(let i = arr.length - 1; i > 0; i--) {
-        // pick random index before current element
-        let j = Math.floor(Math.random() * (i + 1))
-        // swap
-        [arr[i], arr[j]] = [arr[j], [arr[i]]]
-        console.log(arr);
     }
 }
