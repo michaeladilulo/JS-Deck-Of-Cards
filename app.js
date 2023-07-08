2799de1b1a47656f4841840a9bd7b12ab2868191
michaeladilulo <michaeladilulo@gmail.com>
2023-07-07 19:23:43 -0600

    Loops over suits and values, pushing deck array

---
+++ app.js
@@ -0,0 +1,14 @@
+function makeDeck() {
+    const deck = [];
+    const suits = ['hearts', 'diamonds', 'clubs', 'spades'];
+    const values = '1,2,3,4,5,6,7,8,9,10,J,Q,K,A'
+
+    for(let value of values.split(',')) {
+        for(let suit of suits) {
+            deck.push({
+                value: value,
+                suit: suit
+            })
+        }
+    }
+}
