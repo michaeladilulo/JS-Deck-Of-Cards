d2c0cb75e528fb272b9307c53c3eab371b01e37f
michaeladilulo <michaeladilulo@gmail.com>
2023-07-07 19:33:54 -0600

    Object methods and this keyword

--- app.js
+++ app.js
@@ -20,3 +20,23 @@ function drawCard(deck) {
 
 const myDeck = makeDeck();
 const card1 = drawDeck(myDeck);
+
+// refactor of function above with object 
+const myNewDeck = {
+    deck: [],
+    suits: ['hearts', 'spades', 'diamonds', 'clubs'],
+    values: '2,3,4,5,6,7,8,9,10,J,Q,K,A',
+        initializeDeck() {
+            const {suits, values, deck} = this;
+
+            for(let value of values.split('')) {
+                for(let suit of suits) {
+                    deck.push({
+                        value,
+                        suit,
+                        deck
+                    })
+                }
+            }
+        }
+}
